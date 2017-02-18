4d0e97f92d3a6e7d7f7851b553235a60a55d5bc8
Thomas CARTON <thomas.carton@palto.com>
2017-02-19 00:34:55 +0100

    improved to execute the first sample code

--- synacor.c
+++ synacor.c
@@ -7,20 +7,37 @@
 #include <memory.h>
 
 
-unsigned int reg[8];
-unsigned int mem[32768];
-unsigned int pc;
+unsigned short reg[8];
+unsigned short mem[32768];
+unsigned short pc;
+
+
+unsigned short value(unsigned short v)
+{
+	if (v < 32768)
+	{
+		return v;
+	}
+	else if (v < 32776)
+	{
+		return reg[v - 32768];
+	}
+
+	printf("ERR! unallowed value! [pc:%04d = %d]\n", pc, v);
+
+	return 0;
+}
 
 void start()
 {
 	pc = 0;
 
-	unsigned int a, b, c, i;
+	unsigned short a, b, c, i;
 	
 	unsigned int active = 1;
 	while (active)
 	{
-		i = mem[pc]; pc += 2;
+		i = mem[pc++];
 
 		switch (i)
 		{
@@ -28,10 +45,33 @@ void start()
 				active = 0;
 				break;
 
-			case 19: // out 19 a
-				a = mem[pc]; pc += 2;
-				printf("%c", a);
+			case 9: // add 9 a b c
+			{
+				a = mem[pc++];
+				b = mem[pc++];
+				c = mem[pc++];
+
+				unsigned short tmp = value(b) + value(c);
+
+				if (a < 32768)
+				{
+					mem[a] = tmp & 32767;
+				}
+				else if (a < 32776)
+				{
+					reg[a - 32768] = tmp & 32767;
+				}
+			
 				break;
+			}
+
+			case 19: // out 19 a
+			{
+				a = mem[pc++];
+				printf("%d\n", value(a));
+
+				break;
+			}
 
 			case 21: // noop 21
 				break;
@@ -46,7 +86,7 @@ void start()
 
 int main(int argc, const char *argv[])
 {
-	unsigned int prg[] = { 9, 32768, 32769, 4, 19, 32768 };
+	unsigned short prg[] = { 9, 32768, 32769, 4, 19, 32768 };
 
 	memcpy(mem, prg, sizeof(prg));
 	start();
