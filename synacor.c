7a42b2df467377ff24c8420f3eeaca51bed374fe
Thomas CARTON <thomas.carton@palto.com>
2017-02-19 09:23:04 +0100

    Added debug mode to display decoded instructions

--- synacor.c
+++ synacor.c
@@ -3,8 +3,10 @@
 // written by Thomas CARTON
 //
 
-#include <stdio.h>
 #include <memory.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 
 typedef unsigned short word;
@@ -14,6 +16,8 @@ word mem[32768];
 
 word pc;
 word a, b, c;
+
+bool debug = false;
 
 
 word value(word v)
@@ -48,21 +52,82 @@ void store(word v, word dest)
 	}
 }
 
+void dumpInstruction(const unsigned int addr, const char *inst, const unsigned int count, ...)
+{
+	va_list ap;
+
+	// memory
+	unsigned int p = addr - count - 1;
+	fprintf(stderr, "\n%08X: %04X", p * (unsigned int)sizeof(word), mem[p]);
+	for (unsigned int i = 0; i < count; ++i)
+	{
+		fprintf(stderr, " %04X", mem[p + i]);
+	}
+
+	// instruction
+    char pad[3 * 5] = "               ";
+    pad[(3 - count) * 5] = '\0';
+	fprintf(stderr, "%s\t%s", pad, inst);
+
+	// operands
+	va_start(ap, count);
+	for (unsigned int i = 0; i < count; ++i)
+	{
+		word o = va_arg(ap, int);
+
+		if (o < 32768)
+		{
+			fprintf(stderr, " %d", o);
+		}
+		else if (o < 32776)
+		{
+			fprintf(stderr, " r%d", o & 7);
+		}
+	}
+	va_end(ap);
+}
+
 void run()
 {
 	pc = 0;
-
 	
-	unsigned int active = 1;
+	bool active = true;
 	while (active)
 	{
 		switch (mem[pc++])
 		{
 			case 0: // halt: 0
 			{
-				active = 0;
+				if (debug) dumpInstruction(pc, "HALT", 0);
+
+				active = false;
 
 				fprintf(stderr, ">>HALT: Program ended. [pc:%04d = %d]\n", pc, mem[pc - 1]);
+
+				break;
+			}
+
+			case 1: // set: 1 a b
+			{
+				a = mem[pc++];
+				b = mem[pc++];
+
+				if (debug) dumpInstruction(pc, "SET", 2, a, b);
+
+				store(b, a);
+
+				break;
+			}
+
+			case 4: // eq: 4 a b c
+			{
+				a = mem[pc++];
+				b = mem[pc++];
+				c = mem[pc++];
+
+				if (debug) dumpInstruction(pc, "EQ", 3, a, b, c);
+
+				store(a, value(b) == value(c) ? 1 : 0);
 
 				break;
 			}
@@ -71,13 +136,22 @@ void run()
 			{
 				a = mem[pc++];
 
+				if (debug) dumpInstruction(pc, "JUMP", 1, a);
+
 				if (a < 32768)
 				{
-					pc = a;
+					if (a & 1)
+					{
+						fprintf(stderr, ">>WARN! invalid JUMP address! [pc:%04d = %d]\n", pc, a);
+						a &= ~1;
+						break;
+					}
+
+					pc = a >> 1;
 				}
 				else
 				{
-					fprintf(stderr, "\n\n>>ERR! invalid JUMP address! [pc:%04d = %d]\n", pc, a);
+					fprintf(stderr, ">>ERR! invalid JUMP address! [pc:%04d = %d]\n", pc, a);
 				}
 
 				break;
@@ -88,7 +162,9 @@ void run()
 				a = mem[pc++];
 				b = mem[pc++];
 
-				if (a)
+                if (debug) dumpInstruction(pc, "JT", 2, a, b);
+
+				if (value(a))
 				{
 					if (b < 32768)
 					{
@@ -108,7 +184,9 @@ void run()
 				a = mem[pc++];
 				b = mem[pc++];
 
-				if (a == 0)
+                if (debug) dumpInstruction(pc, "JF", 2, a, b);
+
+				if (value(a) == 0)
 				{
 					if (b < 32768)
 					{
@@ -129,6 +207,8 @@ void run()
 				b = mem[pc++];
 				c = mem[pc++];
 
+                if (debug) dumpInstruction(pc, "ADD", 3, a, b, c);
+
 				store((value(b) + value(c)) % 32767, a);
 			
 				break;
@@ -139,6 +219,8 @@ void run()
 				a = mem[pc++];
 				b = mem[pc++];
 				c = mem[pc++];
+
+                if (debug) dumpInstruction(pc, "AND", 3, a, b, c);
 
 				store(value(b) & value(c), a);
 
@@ -151,6 +233,8 @@ void run()
 				b = mem[pc++];
 				c = mem[pc++];
 
+                if (debug) dumpInstruction(pc, "OR", 3, a, b, c);
+
 				store(value(b) | value(c), a);
 
 				break;
@@ -161,6 +245,9 @@ void run()
 				a = mem[pc++];
 				b = mem[pc++];
 
+                if (debug) dumpInstruction(pc, "NOT", 2, a, b);
+
+
 				store(~value(b), a);
 
 				break;
@@ -170,21 +257,24 @@ void run()
 			{
 				a = mem[pc++];
 
-				fprintf(stdout, "%d", value(a));
+				if (debug) dumpInstruction(pc, "OUT", 1, a);
+
+				fprintf(stdout, "%c", value(a));
 
 				break;
 			}
 
 			case 21: // noop: 21
 			{
+				if (debug) dumpInstruction(pc, "NOOP", 0);
+
 				break;
 			}
 
 			default:
 			{
 				fprintf(stderr, ">>ERR! unrecognized instruction! [pc:%04d = %d]\n", pc, mem[pc - 1]);
-
-				break;
+				return;
 			}
 		}
 	}
@@ -201,7 +291,7 @@ int readFile(const char *filename)
         return -1;
     }
 
-	int sizeread = fread(mem, 2, 32768, f);
+	int sizeread = (int)fread(mem, 2, 32768, f);
 
     fclose(f);
 
@@ -210,8 +300,24 @@ int readFile(const char *filename)
 
 int main(int argc, const char *argv[])
 {
-	if (argc != 2)
-    	goto usage;
+	if (argc < 2 || argc > 3)
+	   	goto usage;
+
+    // read parameters
+    for (unsigned int i = 0; i < argc; ++i)
+    {
+        // debug
+        if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--debug") == 0)
+        {
+        	debug = true;
+        }
+
+        // help
+        else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
+        {
+            goto usage;
+        }
+    }
 
 	if (readFile(argv[1]) > 0)
 	{
@@ -228,7 +334,11 @@ usage:
     fprintf(stderr, "   -------\n\n");
 
     fprintf(stderr, "   usage:\n");
-    fprintf(stderr, "     %s path\n\n", argv[0]);
+    fprintf(stderr, "     %s path [-h/--help] | [-d/--debug]\n\n", argv[0]);
+
+    fprintf(stderr, "   parameters:\n");
+    fprintf(stderr, "     -h, --help     - display this\n");
+    fprintf(stderr, "     -d, --debug     - enable debug\n\n");
 
     fprintf(stderr, "   example:\n");
     fprintf(stderr, "     %s example.bin\n", argv[0]);
